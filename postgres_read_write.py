4419481be98c28c668710de3a3bd5f10a0a19cd3
AdamSpindrift <adam@spindriftprojects.com>
2023-01-18 14:40:47 +0100

    Connection to local db working

--- postgres_read_write.py
+++ postgres_read_write.py
@@ -1,11 +1,28 @@
 import psycopg2
+from dotenv.main import load_dotenv
+import os
+
+load_dotenv()
 
 def db_read_write(host,db,user,password,port):
     
-    conn = psycopg2.connect(
-        host = host,
-        database = db,
-        user = user,
-        password = password,
-        port = port,
-    )
+    conn = psycopg2.connect(f"dbname={db} user={user} password={password} port={port} host={host}")
+    
+    cursor = conn.cursor()
+    
+    cursor.execute('SELECT * FROM information_schema.tables')
+    rows = cursor.fetchall()
+    for table in rows:
+        print(table)
+        
+        
+    conn.close()
+    
+host = os.environ["POSTGRES_HOST"]
+database = "wallapopnintendodb"
+user = os.environ["POSTGRES_USER"]
+password = os.environ["POSTGRES_PASSWORD"]
+port = os.environ["POSTGRES_PORT"]
+
+
+db_read_write(host,database,user,password,port)
