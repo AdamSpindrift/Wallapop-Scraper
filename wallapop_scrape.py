e938118532fe2103edb505683a903f2d77a4922c
AdamSpindrift <adam@spindriftprojects.com>
2023-01-24 15:40:01 +0100

    Error Handling for Selenium

--- wallapop_scrape.py
+++ wallapop_scrape.py
@@ -4,6 +4,15 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 import time
+from telegram_bot import send_telegram
+from dotenv.main import load_dotenv
+import os
+
+load_dotenv()
+
+# Telegram Environment Variables
+token = os.environ["TELEGRAM_TOKEN"]
+chat_id = os.environ["CHAT_ID"]
 
 def wallapopScrape(url):
     
@@ -15,56 +24,66 @@ def wallapopScrape(url):
     driver.maximize_window()
     driver.implicitly_wait(20)
 
-    driver.get(url)
+    try:
+        
+        driver.get(url)
 
-    driver.find_element(By.ID,"onetrust-accept-btn-handler").click()
-    time.sleep(5)
-    driver.find_element(By.CSS_SELECTOR,"#btn-load-more > button").click()
-    time.sleep(5)
+        driver.find_element(By.ID,"onetrust-accept-btn-handler").click()
+        time.sleep(5)
+        driver.find_element(By.CSS_SELECTOR,"#btn-load-more > button").click()
+        
+    except(Exception):
+        print("An error occurred loading the web page")
+        send_telegram(token,chat_id,"An error occurred loading the web page")
+        return([])
+        
+    finally:
+        
+        time.sleep(5)
 
-    html = driver.page_source
+        html = driver.page_source
 
-    time.sleep(10)
-    driver.quit()
+        time.sleep(10)
+        driver.quit()
 
-    soup = BeautifulSoup(html,"html.parser")
+        soup = BeautifulSoup(html,"html.parser")
 
-    items = soup.find_all("div", class_="ItemCard__data--with-description")
-    price = soup.find_all("span", class_="ItemCard__price--bold")
-    description = soup.find_all("p", class_="ItemCard__title")
-    img = soup.find_all("img",class_="ng-star-inserted")
-
-
-    extracted_prices = []
-    extracted_descriptions = []
-    extracted_img = []
-    extracted_items = []
-
-
-    for p in price:
-        extracted_prices.append(p.get_text())
-
-    for d in description:
-        extracted_descriptions.append(d.get_text())
-
-    for s in img:
-        extracted_img.append(s["src"])
+        items = soup.find_all("div", class_="ItemCard__data--with-description")
+        price = soup.find_all("span", class_="ItemCard__price--bold")
+        description = soup.find_all("p", class_="ItemCard__title")
+        img = soup.find_all("img",class_="ng-star-inserted")
 
 
-    time.sleep(5)
+        extracted_prices = []
+        extracted_descriptions = []
+        extracted_img = []
+        extracted_items = []
 
 
-    for idx, i in enumerate(extracted_prices):
-        p = i
-        d = extracted_descriptions[idx]
-        image_url = extracted_img[idx]
-        b = {
-            "description":d,
-            "price":p,
-            "img":image_url,}
+        for p in price:
+            extracted_prices.append(p.get_text())
 
-        extracted_items.append(b.copy())
+        for d in description:
+            extracted_descriptions.append(d.get_text())
 
-    time.sleep(5)
+        for s in img:
+            extracted_img.append(s["src"])
 
-    return(extracted_items)
+
+        time.sleep(5)
+
+
+        for idx, i in enumerate(extracted_prices):
+            p = i
+            d = extracted_descriptions[idx]
+            image_url = extracted_img[idx]
+            b = {
+                "description":d,
+                "price":p,
+                "img":image_url,}
+
+            extracted_items.append(b.copy())
+
+        time.sleep(5)
+
+        return(extracted_items)
